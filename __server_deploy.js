d39af29876f5a9c65739820a4f9a2d35b4a09b7f
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-02 02:51:00 +0600

    Adding steps

---
+++ __server_deploy.js
@@ -0,0 +1,13 @@
+/* 
+-------------------
+* One Time
+-------------------
+1. Create HeroKu account
+2. Verify email
+3. install heroku cli
+4. heroku login
+
+----------------------
+* For each project
+1. Heroku create
+*/
